935dc660ffc8821143d15705ef8bab4d9e41806c
Georgi <gtyufekchiev@gmail.com>
2021-05-06 16:33:08 +0300

    Using a sound method to test the arrow sound

--- src/main/java/lol/client/ai/RandomAI.java
+++ src/main/java/lol/client/ai/RandomAI.java
@@ -3,15 +3,15 @@ package lol.client.ai;
 import java.util.*;
 import lol.game.*;
 import lol.game.action.*;
-
+import lol.ui.Sound;
 public class RandomAI extends AIBase {
   private Random random;
-
+  private Sound sound;
   public RandomAI(Arena arena, Battlefield battlefield) {
     super(arena, battlefield);
     random = new Random();
+    this.sound = new Sound();
   }
-
   public Turn championSelect() {
     Turn turn = new Turn();
     String championName;
@@ -33,10 +33,11 @@ public class RandomAI extends AIBase {
       battlefield.visitAdjacent(champion.x(), champion.y(), champion.attackRange(), new TileVisitor(){
         public void visitNexus(Nexus nexus) {
           if(nexus.teamOfNexus() != teamID) {
+            sound.playSound();            
             turn.registerAction(new Attack(teamID, id, nexus.x(), nexus.y()));
           }
         }
-      }));
+      }));     
     // Add a move action in case we could not attack the Nexus.
     arena.teamOf(teamID).forEachChampion((champion, id) ->
       battlefield.visitAdjacent(champion.x(), champion.y(), champion.walkSpeed(), new TileVisitor(){
