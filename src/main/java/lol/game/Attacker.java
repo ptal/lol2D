fc3af2600c7c30a4d0b88440a68dae98efde845a
Gwentinho <qschroed@gmail.com>
2021-05-06 18:04:40 +0200

    Added new attacker abstract class to differentiate towers and champions

---
+++ src/main/java/lol/game/Attacker.java
@@ -0,0 +1,35 @@
+package lol.game;
+
+
+// class is abstract, since it should never be used alone
+public abstract class Attacker extends Destructible {
+    private int rangeOfAttack;
+    private int damages;
+
+    public Attacker(int hp, int rangeOfAttack, int damages) {
+        super(hp);
+        this.rangeOfAttack = rangeOfAttack;
+        this.damages = damages;
+    }
+
+    public int attackRange() {
+        return rangeOfAttack;
+    }
+
+    public boolean attack(Destructible d) {
+        if(canAttack(d.x(), d.y())) {
+            d.hit(damages);
+            return true;
+        }
+        return false;
+    }
+
+    protected int distanceFrom(int toX, int toY) {
+        return Math.max(Math.abs(toX - x()), Math.abs(toY - y()));
+    }
+
+
+    public boolean canAttack(int toX, int toY) {
+        return distanceFrom(toX, toY) <= rangeOfAttack;
+    }
+}
--- src/main/java/lol/game/Tower.java
+++ src/main/java/lol/game/Tower.java
@@ -1,45 +1,21 @@
 package lol.game;
 
-public class Tower extends Destructible{
+public class Tower extends Attacker{
     public static final int HP_TOWER = 5;
     public static final int RANGE_OF_ATTACK = 2;
     public static final int DAMAGE_TOWER = 4;
     public static final int BLUE = 0;
     public static final int RED = 1;
     private int teamID;
-    private int rangeOfAttack;
-    private int damages;
 
     // we let the attributes be constants for now
     public Tower(int teamID) {
-        super(HP_TOWER);
+        super(HP_TOWER, RANGE_OF_ATTACK, DAMAGE_TOWER);
         this.teamID = teamID;
-        this.rangeOfAttack = RANGE_OF_ATTACK;
-        this.damages = DAMAGE_TOWER;
     }
 
     public int teamOfTower() {
         return teamID;
-    }
-
-    public int attackRange() {
-        return rangeOfAttack;
-    }
-
-    private int distanceFrom(int toX, int toY) {
-        return Math.max(Math.abs(toX - x()), Math.abs(toY - y()));
-    }
-
-    public boolean canAttack(int toX, int toY) {
-        return distanceFrom(toX, toY) <= rangeOfAttack;
-    }
-
-    public boolean attack(Destructible d) {
-        if(canAttack(d.x(), d.y())) {
-            d.hit(damages);
-            return true;
-        }
-        return false;
     }
 
     public String name() {
