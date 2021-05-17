d0e418c5493cdb03af89aca8de038c929dd74e52
sena <sena@sena.fritz.box>
2021-05-17 19:51:08 +0300

    Spell class now extends Action class.

--- src/main/java/lol/game/action/Spell.java
+++ src/main/java/lol/game/action/Spell.java
@@ -3,7 +3,7 @@ package lol.game.action;
 import lol.game.*;
 import java.io.Serializable;
 
-public abstract class Spell extends ChampionAction implements Serializable {
+public abstract class Spell extends Action implements Serializable {
 
   public Spell(int teamID, int championID, int x, int y) {
     super(teamID,championID,x,y);
