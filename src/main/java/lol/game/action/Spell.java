56ed156e5dfd74ea4b662b0a46551833969f3f65
zofiamilczarek <milczarekzofia01@gmail.com>
2021-05-17 19:25:29 +0200

    Spell constructor edited

--- src/main/java/lol/game/action/Spell.java
+++ src/main/java/lol/game/action/Spell.java
@@ -4,8 +4,10 @@ import lol.game.*;
 import java.io.Serializable;
 
 public abstract class Spell extends Action implements Serializable {
+  protected int championID;
 
-  public Spell(int teamID) {
+  public Spell(int teamID, int championID) {
     super(teamID);
+    this.championID=championID;
   }
 }
