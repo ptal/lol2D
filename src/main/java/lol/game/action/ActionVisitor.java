de2ed0f3760724896e0a74a52bcb4c81f049ac57
sena <sena@sena.fritz.box>
2021-05-17 20:46:30 +0300

    Added accept method to DoubleDamage class & added visitDoubleDamage method to ActionVisitor class

--- src/main/java/lol/game/action/ActionVisitor.java
+++ src/main/java/lol/game/action/ActionVisitor.java
@@ -5,4 +5,5 @@ public interface ActionVisitor {
   public void visitMove(int teamID, int championID, int x, int y);
   public void visitAttack(int teamID, int championID, int x, int y);
   public void visitChampionSelect(int teamID, String championName);
+  public void visitDoubleDamage(int teamID, int championID, int x, int y);
 }
--- src/main/java/lol/game/action/DoubleDamage.java
+++ src/main/java/lol/game/action/DoubleDamage.java
@@ -4,8 +4,16 @@ import lol.game.*;
 import java.io.Serializable;
 
 public abstract class DoubleDamage extends Spell implements Serializable {
+  protected int x;
+  protected int y;
 
-  public Spell(int teamID, int championID) {
+  public Spell(int teamID, int championID, int x, int y) {
     super(teamID,championID);
+    this.x = x;
+    this.y = y;
+  }
+
+  public void accept(ActionVisitor visitor) {
+    visitor.visitAttack(teamID, championID, x, y);
   }
 }
