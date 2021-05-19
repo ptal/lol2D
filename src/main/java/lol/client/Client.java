cacc0f9ee641251aa934fa42ed93cbda479f7944
sena <sena@sena.fritz.box>
2021-05-19 21:08:22 +0300

    Updated Client class to test something

--- src/main/java/lol/client/Client.java
+++ src/main/java/lol/client/Client.java
@@ -15,7 +15,7 @@ public class Client implements Runnable {
     ASCIIBattlefieldBuilder battlefieldBuilder = new ASCIIBattlefieldBuilder();
     Battlefield battlefield = battlefieldBuilder.build();
     Arena arena = new Arena(battlefield);
-    RandomAISpell ai = new RandomAISpell(arena, battlefield);
+    RandomAITower ai = new RandomAITower(arena, battlefield);
     new Client(ai, arena, battlefield).run();
   }
 
