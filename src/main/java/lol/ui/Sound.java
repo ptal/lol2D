4b2d94488b466b44616c3a1b2d46e77ad7bb7282
Georgi <gtyufekchiev@gmail.com>
2021-05-19 11:04:50 +0300

    Implemented method for opening the sound path file

--- src/main/java/lol/ui/Sound.java
+++ src/main/java/lol/ui/Sound.java
@@ -9,6 +9,7 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 //Source: Sound from Zapsplat.com
 //Mp3 is not supported
@@ -16,24 +17,36 @@ public class Sound  {
    private File shootArrow;
    private File swordHit;
    private File explodeBuilding;
-   public Sound(){
-      this.shootArrow = new File("src\\main\\resources\\attack_sounds\\arrow.wav");
-      this.swordHit = new File("src\\main\\resources\\attack_sounds\\sword.wav");
-      this.explodeBuilding = new File("src\\main\\resources\\attack_sounds\\explode.wav");
+   public Sound(){      
+      this.shootArrow = openSoundFile("attack_sounds/arrow.wav");
+      this.swordHit = openSoundFile("attack_sounds/sword.wav");
+      this.explodeBuilding = openSoundFile("attack_sounds/explode.wav");
    }
    public void attackSound(String entity){
       switch (entity.toLowerCase()) {
-         case "archer":playSound(this.shootArrow);break;
-         case "warrior":playSound(this.swordHit);break;      
-         default:throw new RuntimeException("The sound for " + entity + " doesn't exist");            
+         case "archer":
+            playSound(this.shootArrow);
+            break;
+         case "warrior":
+            playSound(this.swordHit);
+            break;      
+         default:
+            throw new RuntimeException("The sound for " + entity + " doesn't exist");            
       }
    } 
    public void destroyBuilding(){
       playSound(this.explodeBuilding);
+   }
+   private File openSoundFile(String soundPath) {
+      File soundFile = new File(getClass().getClassLoader().getResource(soundPath).getFile());
+      if(soundFile == null) {
+        throw new RuntimeException("The sound file `" + soundPath + "` could not be found.");
+      }
+      return soundFile;
    }  
    private void playSound(File sound){
-      try {                 
-         AudioInputStream audioIn = AudioSystem.getAudioInputStream(sound.toURI().toURL());         
+      try {                        
+         AudioInputStream audioIn = AudioSystem.getAudioInputStream(sound);         
          Clip clip = AudioSystem.getClip();         
          clip.open(audioIn);
          setVolume(clip);
@@ -50,7 +63,7 @@ public class Sound  {
    }  
    private void setVolume(Clip clip){
       FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-      gainControl.setValue(-10.0f);
+      gainControl.setValue(-15.0f);//decibels
    }
 
 }
