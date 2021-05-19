8183a90efb6e93b0d2a9d60cac3fb4f75f02b35e
zofiamilczarek <milczarekzofia01@gmail.com>
2021-05-19 19:15:05 +0200

    AI class for spell added

---
+++ src/main/java/lol/client/ai/RandomAISpell.java
@@ -0,0 +1,35 @@
+package lol.client.ai;
+
+import java.util.*;
+import lol.game.*;
+import lol.game.action.*;
+
+public class RandomAISpell extends RandomAI {
+
+  public RandomAISpell(Arena arena, Battlefield battlefield) {
+    super(arena, battlefield);
+  }
+
+  public Turn turn() {
+    Turn turn = new Turn();
+    // Try to attack the Nexus first.
+    tryAttackNexus(turn);
+    // Try to attack a Tower.
+    tryDoubleAttack(turn);
+    // Add a move action in case we could not attack the Nexus.
+    tryMove(turn);
+    return turn;
+  }
+
+  private void tryDoubleDamageOnTower(Turn turn) {
+    arena.teamOf(teamID).forEachChampion((champion, id) ->
+      battlefield.visitAdjacent(champion.x(), champion.y(), champion.attackRange(), new TileVisitor(){
+        public void visitTower(Tower tower) {
+        if(tower.teamOfTower() != teamID) {
+          turn.registerAction(new DoubleDamage(teamID, id, tower.x(), tower.y()));
+        }
+        }
+      }));
+  }
+
+}
