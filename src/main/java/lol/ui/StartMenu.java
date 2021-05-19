f14520c71a1a0ec5828f479820e1e60e8d017bd2
PedroS235 <PedroS235@github.com>
2021-05-19 11:25:12 +0200

    Updated the code to work with a stylesheet for customisation

--- src/main/java/lol/ui/StartMenu.java
+++ src/main/java/lol/ui/StartMenu.java
@@ -1,21 +1,24 @@
 package lol.ui;
 
 import javafx.application.Application;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
-import javafx.stage.Stage;
-import javafx.application.Platform;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.geometry.Pos;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+
+import javafx.stage.Stage;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-
+import javafx.geometry.Insets;
 
 import lol.game.*;
 import lol.server.*;
@@ -31,37 +34,36 @@ public class StartMenu extends Application {
 
   @Override
   public void start(Stage stage) {
-    Button startBtn = new Button();
-    startBtn.setText("Start game");
-    startBtn.setOnAction(new EventHandler<ActionEvent>() {
+    stage.setTitle("LOL 2D");
+    GridPane grid = new GridPane();
+    grid.setAlignment(Pos.CENTER);
+    
+    //play button
+    Button startbtn = new Button("Play");
+    grid.add(startbtn, 2, 2);
 
-      @Override
-      public void handle(ActionEvent event) {
-        ASCIIBattlefieldBuilder battlefieldBuilder = new ASCIIBattlefieldBuilder();
-        battlefield = battlefieldBuilder.build();
-        battlefieldView = new BattlefieldView(battlefield, stage);
-        update();
-        stage.show();
-        startServer();
-      }
+    //play button action when pressed (start the server)
+    startbtn.setOnAction(e -> {
+      ASCIIBattlefieldBuilder battlefieldBuilder = new ASCIIBattlefieldBuilder();
+      battlefield = battlefieldBuilder.build();
+      battlefieldView = new BattlefieldView(battlefield, stage);
+      update();
+      stage.show();
+      startServer();
     });
 
-    StackPane root = new StackPane();
-    root.getChildren().add(startBtn);
-
-    Scene scene = new Scene(root, 300, 250);
-
-    stage.setTitle("LOL 2D");
+    Scene scene = new Scene(grid, 660, 360);
     stage.setScene(scene);
+    scene.getStylesheets().add(getClass().getClassLoader().getResource("styles/style.css").toExternalForm());
     stage.show();
   }
 
   public void update() {
-      battlefieldView.update();
+    battlefieldView.update();
   }
 
   private void startServer() {
-      Thread t = new Thread(new Server(this, battlefield));
-      t.start();
+    Thread t = new Thread(new Server(this, battlefield));
+    t.start();
   }
 }
