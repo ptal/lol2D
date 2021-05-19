5f1cca16abfcf12a0be2ae14d757dd3a588a7f79
Titouan Guerin <titouanguerin@gmail.com>
2021-05-19 21:22:01 +0200

    added gif for winner screen

--- src/main/java/lol/ui/WinnerScreen.java
+++ src/main/java/lol/ui/WinnerScreen.java
@@ -9,10 +9,15 @@ public class WinnerScreen {
 
         // the panel with the button and text
         JLabel label = new JLabel("The winner is team " + winner + "!");
+        label.setFont(new Font("Verdana", Font.PLAIN, 30));
         label.setIconTextGap(10);
         JPanel panel = new JPanel();
 
-        panel.setBorder(BorderFactory.createEmptyBorder(150, 150, 150, 150));
+        ImageIcon icon = new ImageIcon("crown-winner.gif");
+
+        label.setIcon(icon);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
         panel.setLayout(new GridLayout(0, 1));
         if(winner.equals("Blue")) {
             panel.setBackground(Color.blue);
@@ -22,12 +27,11 @@ public class WinnerScreen {
         }
         panel.add(label);
 
-
-
         // set up the frame and display it
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Winner Screen");
+        frame.setSize(300,300);
         frame.pack();
         frame.setVisible(true);
     }
