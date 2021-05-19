6a7a09e09f588795c85816ef10bb71824117948a
barbara <barbarasymeonc@gmail.com>
2021-05-20 01:40:16 +0200

    Config for champion, monster, nexus, tower

---
+++ src/main/java/lol/config/Config.java
@@ -0,0 +1,32 @@
+package lol.config;
+
+public class Config {
+
+  public static final int HP_NEXUS = 25;
+
+  public static final int HP_ARCHER = 5;
+  public static final int RANGE_OF_ATTACK_ARCHER = 2;
+  public static final int DAMAGE_ARCHER = 4;
+  public static final int SPEED_ARCHER = 4;
+
+  public static final int HP_WARRIOR = 5;
+  public static final int RANGE_OF_ATTACK_WARRIOR = 2;
+  public static final int DAMAGE_WARRIOR = 4;
+  public static final int SPEED_WARRIOR = 4;
+
+  public static final int HP_TOWER = 5;
+  public static final int RANGE_OF_ATTACK_TOWER = 2;
+  public static final int DAMAGE_TOWER = 4;
+
+  public static final int HP_DRAGON = 10;
+  public static final int RANGE_OF_ATTACK_DRAGON = 5;
+  public static final int DAMAGE_DRAGON = 5;
+
+  public static final int HP_NASHOR = 5;
+  public static final int RANGE_OF_ATTACK_NASHOR = 2;
+  public static final int DAMAGE_NASHOR = 4;
+
+  public static final int BOOST_DAMAGES = 5;
+  public static final int BOOST_SPEED = 5;
+
+}
