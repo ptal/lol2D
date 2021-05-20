9608bbf35211f96f75298738db8061aab434702d
Barbara MMCS <51007181+BarbaraMMCS@users.noreply.github.com>
2021-05-20 10:43:18 +0200

    Update Config.java

--- src/main/java/lol/config/Config.java
+++ src/main/java/lol/config/Config.java
@@ -4,27 +4,27 @@ public class Config {
 
   public static final int HP_NEXUS = 25;
 
-  public static final int HP_ARCHER = 5;
-  public static final int RANGE_OF_ATTACK_ARCHER = 2;
-  public static final int DAMAGE_ARCHER = 4;
-  public static final int SPEED_ARCHER = 4;
+  public static final int HP_ARCHER = 10;
+  public static final int RANGE_OF_ATTACK_ARCHER = 3;
+  public static final int DAMAGE_ARCHER = 2;
+  public static final int SPEED_ARCHER = 2;
 
-  public static final int HP_WARRIOR = 5;
-  public static final int RANGE_OF_ATTACK_WARRIOR = 2;
-  public static final int DAMAGE_WARRIOR = 4;
-  public static final int SPEED_WARRIOR = 4;
+  public static final int HP_WARRIOR = 15;
+  public static final int RANGE_OF_ATTACK_WARRIOR = 1;
+  public static final int DAMAGE_WARRIOR = 3;
+  public static final int SPEED_WARRIOR = 2;
 
   public static final int HP_TOWER = 5;
   public static final int RANGE_OF_ATTACK_TOWER = 2;
   public static final int DAMAGE_TOWER = 4;
 
   public static final int HP_DRAGON = 10;
-  public static final int RANGE_OF_ATTACK_DRAGON = 5;
+  public static final int RANGE_OF_ATTACK_DRAGON = 3;
   public static final int DAMAGE_DRAGON = 5;
 
-  public static final int HP_NASHOR = 5;
-  public static final int RANGE_OF_ATTACK_NASHOR = 2;
-  public static final int DAMAGE_NASHOR = 4;
+  public static final int HP_NASHOR = 15;
+  public static final int RANGE_OF_ATTACK_NASHOR = 3;
+  public static final int DAMAGE_NASHOR = 5;
 
   public static final int BOOST_DAMAGES = 5;
   public static final int BOOST_SPEED = 5;
